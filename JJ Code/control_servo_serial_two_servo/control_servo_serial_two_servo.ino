ddfc962c8d0492f512033eae638c933c65885ff8
lujan002 <20JansenLu@gmail.com>
2024-04-26 12:56:19 -0500

    updating packages install guide

--- JJ Code/control_servo_serial_two_servo/control_servo_serial_two_servo.ino
+++ JJ Code/control_servo_serial_two_servo/control_servo_serial_two_servo.ino
@@ -1,37 +1,22 @@
-#include <Servo.h>
+const int limitSwitchPin = 2; // Pin connected to the limit switch
 
-Servo servo1;
-Servo servo2;
-// Add more servos as needed
+volatile int switchState = HIGH; // volatile keyword is used as this variable changes inside an interrupt
 
 void setup() {
   Serial.begin(9600);
-  servo1.attach(10);
-  servo1.write(0);
-  servo2.attach(11);
-  servo2.write(180);  
-  //Attach more servos as needed
+  pinMode(limitSwitchPin, INPUT_PULLUP); // Use internal pull-up resistor
+  attachInterrupt(digitalPinToInterrupt(limitSwitchPin), toggleSwitch, CHANGE); // Attach interrupt
 }
 
 void loop() {
-  // Check if data is available to read from the serial buffer
-  if (Serial.available()) {
-    // Read the incoming data as a string until a newline character ('\n') is encountered
-    String data = Serial.readStringUntil('\n');
-
-    int firstCommaIndex = data.indexOf(',');
-
-    // Extract servo values from the data string
-    int servo1Value = data.substring(0, firstCommaIndex).toInt();
-    int servo2Value = data.substring(firstCommaIndex + 1).toInt();
-
-    // Send the integer values to the respective servos to set their positions
-    servo1.write(servo1Value);
-    servo2.write(servo2Value);
-  
- 
- 
- 
+  if (switchState == LOW) {
+    Serial.println("Low"); // Pressed
+  } else {
+    Serial.println("High"); // Not pressed
   }
+  delay(100); // Delay to reduce serial output speed
 }
 
+void toggleSwitch() {
+  switchState = digitalRead(limitSwitchPin); // Read the new state of the limit switch
+}
